3f20d9edb5493408286cee98280ff9e8cc26fd39
iraja-baltezan <iraja.baltezan@gmail.com>
2021-11-18 17:10:03 -0300

    Exercício 2 concluído. Configuração WebPack

---
+++ ex02/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = ()=>{
+    return (
+        <div>
+            <h2>Este é o App!</h2>
+            <p>Teste</p>
+        </div>
+    )
+}
+
+export default App
